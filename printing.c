d4bb858336f3f51fde92418c7f940e9999438677
Josh Kuhn <habitue@chompy.(none)>
2009-10-16 12:01:27 -0400

    Added ability to completely preprocess out debug statements

--- printing.c
+++ printing.c
@@ -3,15 +3,15 @@
 /* Creates a binary representation of a binary string.  Adapted from:
    http://stackoverflow.com/questions/699968/display-the-binary-representation-of
    -a-number-in-c  */
-void printbits(uint8_t byte)
+
+void __printbits(uint8_t byte)
 {
-#ifdef DEBUG
+
         for(int i = 8; i != 0; --i) putc('0' + ((byte >> (i-1)) & 1), stderr);
-#endif
 }
 
 /* Print out a bitmask table */
-void print_masks(uint8_t ** q_masks, uint64_t height, uint64_t width)
+void __print_masks(uint8_t ** q_masks, uint64_t height, uint64_t width)
 {
         for (uint64_t i = 0; i < height; ++i){
                 for(uint64_t j = 0; j < width; j++){
@@ -24,7 +24,7 @@ void print_masks(uint8_t ** q_masks, uint64_t height, uint64_t width)
 
 /* Prints a contiguous area of memory in binary starting with ptr to an array of
  * the given size */
-void print_mem(uint8_t * start, uint64_t size, uint64_t cols)
+void __print_mem(uint8_t * start, uint64_t size, uint64_t cols)
 {
         for(uint64_t i = size; i != 0; --i){
                 printbits(start[i-1]);
@@ -34,7 +34,7 @@ void print_mem(uint8_t * start, uint64_t size, uint64_t cols)
 }
 
 /* Prints out lookup tables in a readable format */
-void print_tables(uint64_t h, uint64_t d, uint64_t w, uint8_t tables[h][d][w])
+void __print_tables(uint64_t h, uint64_t d, uint64_t w, uint8_t tables[h][d][w])
 {
         for( uint64_t j = 0; j < d; ++j){
                 Trace("Table #%"PRIu64":\n",j+1);
