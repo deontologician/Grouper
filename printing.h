001c15f00505f8e72407fd8822c406bcfa8069a8
Josh Kuhn <habitue@chompy.(none)>
2009-10-16 12:01:33 -0400

    Added ability to completely preprocess out debug statements

--- printing.h
+++ printing.h
@@ -8,16 +8,24 @@
 /* Printing macros */
 #ifdef DEBUG
  #define Trace(...) fprintf(stderr,__VA_ARGS__)
+ #define printbits(...) __printbits(__VA_ARGS__)
+ #define print_masks(...) __print_masks(__VA_ARGS__)
+ #define print_mem(...) __print_mem(__VA_ARGS__)
+#define print_tables(...) __print_tables(__VA_ARGS__)
 #else
- #define Trace(...)
+ #define Trace(...) 
+ #define printbits(...) 
+ #define print_masks(...) 
+ #define print_mem(...) 
+ #define print_tables(...) 
 #endif
 
 #define Error(...) fprintf(stderr,__VA_ARGS__)
 #define Print(...) fprintf(stdout,__VA_ARGS__) 
 
 /* Prototypes */
-void print_tables(uint64_t h, uint64_t d, uint64_t w, uint8_t tables[h][d][w]);
-void printbits(uint8_t byte);
-void print_mem(uint8_t * start, uint64_t size, uint64_t cols);
-void print_masks(uint8_t ** q_masks, uint64_t height, uint64_t width);
+void __print_tables(uint64_t h, uint64_t d, uint64_t w, uint8_t tables[h][d][w]);
+void __printbits(uint8_t byte);
+void __print_mem(uint8_t * start, uint64_t size, uint64_t cols);
+void __print_masks(uint8_t ** q_masks, uint64_t height, uint64_t width);
 
