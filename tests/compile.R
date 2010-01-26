c99ce6d258f1058cfcd4ac8edee49cb0cb48ed23
Josh Kuhn <habitue@chompy.(none)>
2010-01-26 14:13:58 -0500

    Added some more useful processing functions

--- tests/compile.R
+++ tests/compile.R
@@ -1,5 +1,5 @@
-readandprint <- function(bits) {
-  patt <- paste("alltest",bits,"bit_[1-5].csv", sep = "")
+readandprintkbps <- function(bits) {
+  patt <- paste("alltest",bits,"bits_[1-5].csv", sep = "")
   files <- lapply(c(list.files(pattern = patt)), read.csv, head=T)
   total <- data.frame()
   for (file in files[]) {
@@ -27,3 +27,61 @@ readandprint <- function(bits) {
   dev.off()
 }
 
+readandprintpps <- function(bits) {
+  patt <- paste("alltest",bits,"bits_[1-5].csv", sep = "")
+  files <- lapply(c(list.files(pattern = patt)), read.csv, head=T)
+  total <- data.frame()
+  for (file in files[]) {
+    total <- merge(total, file, all=T)
+  }
+  total$number.of.rules <- factor(total$number.of.rules)
+  total$number.of.tables <- factor(total$number.of.tables)
+
+  #split the total into subsets based on the rules
+  vect <- levels(total$number.of.rules)
+  print(vect)
+  x <- list()
+  for (i in 1:length(vect)) {
+    x[[i]] <- subset(total, total$number.of.rules == vect[[i]], all=TRUE)
+  }
+  #print them all to file!
+  filename <- paste("alltest",bits,"bit_pps_vs_rules.pdf", sep="")
+  pdf(file = filename)
+  for (p in x) {
+    title = paste(bits, "bits,", p$number.of.rules[[1]], "Rules")
+    xlabel = "Number of Tables"
+    ylabel = "PPS"
+    plot(p$number.of.tables, p$pps, main = title, xlab = xlabel, ylab = ylabel)
+  }
+  dev.off()
+}
+
+readandprintrealpps <- function(bits) {
+  patt <- paste("alltest",bits,"bits_[1-5].csv", sep = "")
+  files <- lapply(c(list.files(pattern = patt)), read.csv, head=T)
+  total <- data.frame()
+  for (file in files[]) {
+    total <- merge(total, file, all=T)
+  }
+  total$number.of.rules <- factor(total$number.of.rules)
+  total$number.of.tables <- factor(total$number.of.tables)
+
+  #split the total into subsets based on the rules
+  vect <- levels(total$number.of.rules)
+  print(vect)
+  x <- list()
+  for (i in 1:length(vect)) {
+    x[[i]] <- subset(total, total$number.of.rules == vect[[i]], all=TRUE)
+  }
+  #print them all to file!
+  filename <- paste("alltest",bits,"bit_realpps_vs_rules.pdf", sep="")
+  pdf(file = filename)
+  for (p in x) {
+    title = paste(bits, "bits,", p$number.of.rules[[1]], "Rules")
+    xlabel = "Number of Tables"
+    ylabel = "PPS"
+    plot(p$number.of.tables, 500000 / p$real.process.time, main = title,
+         xlab = xlabel, ylab = ylabel)
+  }
+  dev.off()
+}
