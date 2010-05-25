268857e16ecaf0d6627a47a19bf889501c8bb93d
Josh Kuhn <kuhn.joshua@gmail.com>
2010-05-25 12:41:37 -0400

    Graphs now break

--- tests/compile.R
+++ tests/compile.R
@@ -1,4 +1,25 @@
+
 cexsize <- 1.5 #Handles text scaling
+pchvals <- c(3,1,4,5)
+
+device.height <- 7.5
+device.width <- 11
+pdf.device <- function(file, title, height = device.height,
+                       width = device.width) {
+  pdf(file=paste(file,".pdf",sep=""), title = title,
+      width=device.width, height=height, paper="USr")
+}
+
+eps.device <- function(file, title, height = device.height,
+                       width = device.width) {
+  postscript(file = paste(file,".eps",sep=""),
+             width=device.width, height=height, paper="letter",
+             onefile = T,
+             title = title)
+}
+
+printdevice <- eps.device
+
 
 tables.to.bytes <- function(bits, rules, numtables) {
   N <- 8 * ceiling(rules / 8)
@@ -166,11 +187,10 @@ real.pps.vs.tables <- function(pattern
   for(i in 1:length(rsp)){
     numrules <- rsp[[i]]$number.of.rules[1]
     filename <- paste("Tables_vs_PPS_",dot(bits),"bits_",
-                      dot(numrules),"rules.pdf",sep="")
+                      dot(numrules),"rules",sep="")
     titletext <- paste("Throughputs for", comma(bits),
                        "Bits Classified, with", comma(numrules),"Rules")
-    pdf(file=paste(dir,filename,sep="/"), title = titletext,
-        width=11, height=8.5, paper="USr")
+    printdevice(paste(dir,filename,sep="/"), title = titletext)
     par(mar=mar, cex=cexsize, bty="o")
     options(scipen=10)
     plot(x = rsp[[i]]$number.of.tables, y = rsp[[i]]$real.pps,
@@ -267,6 +287,8 @@ real.pps.vs.tables.12K <- function(){
 }
 #really... convenient
 real.pps.vs.tables.all <- function(){
+
+  #real.pps.vs.tables.32()
   #real.pps.vs.tables.104()
   real.pps.vs.tables.320()
   real.pps.vs.tables.12K()
@@ -292,16 +314,14 @@ classifier.throughputs <- function(dir = "./pdfs"){
   rmgd <- lapply(minmaxed, merge.all)
   titletext <- "Classifier Throughputs"
   #draw the plot
-  pdf(paste(dir,"classifier_throughputs.pdf",sep="/"),
-      width=11, height=8.5, paper="USr",
-      title = titletext)
+  printdevice(paste(dir,"classifier_throughputs",sep="/"), titletext)
   options(scipen = 10)
   xlimit <- c(50, mil(1.5))
   ylimit <- c(100, mil(10))
   par(cex=cexsize, bty="u", mar=c(3,3,1,4.5), tcl=-0.3, ann = F)
   plot(x = rmgd[[1]]$number.of.rules, y = rmgd[[1]]$real.pps,
        xlim = xlimit, ylim = ylimit, 
-       log="xy", pch = 1, xaxt="n", yaxt="n", tck = 0
+       log="xy", pch = pchvals[1], xaxt="n", yaxt="n", tck = 0
        )
   pps.ticks <- c(100, thou(1), thou(10), thou(100), mil(1), mil(10))
   bps.ticks <- pps.ticks/1.2 #ticks are in pps scale
@@ -316,10 +336,11 @@ classifier.throughputs <- function(dir = "./pdfs"){
   mtext(2, text = "Throughput (PPS)", cex = cexsize, line = 2)
   axis(4, at = bps.ticks, labels = bps.labels,las=1)
   for(i in 2:length(rmgd)){
-      points(x = rmgd[[i]]$number.of.rules, y = rmgd[[i]]$real.pps, pch = i+1)
+      points(x = rmgd[[i]]$number.of.rules, y = rmgd[[i]]$real.pps,
+             pch = pchvals[i])
   }
   legend(x="bottomleft",legend=c(32,104,320,12000),
-         pch=c(1,3,4,5), title="Bits Classified")
+         pch=pchvals, title="Bits Classified")
   dev.off()
 }
 
@@ -348,15 +369,13 @@ build.time.vs.rules <- function (dir = "./pdfs") {
   rmgd <- lapply(minmaxed, merge.all)
   titletext <- "Table-build Times"
   
-  pdf(paste(dir,"build_time_vs_rules.pdf",sep="/"),
-      height=8.5, width=11, paper="USr",
-      title = titletext)
+  printdevice(paste(dir,"build_time_vs_rules",sep="/"), titletext)
   options(scipen = 10)
   par(cex = 1.5 , bty="l",
       mar=c(3,3,1,1), ann = F)
   plot(x = rmgd[[1]]$number.of.rules, y = rmgd[[1]]$table.build.time, log="xy",
        xlim = c(100,1000000), ylim = c(0.001,2000),
-       pch = 1, axes=T, xaxt="n", yaxt="n", tck=0.0)
+       pch = pchvals[1], axes=T, xaxt="n", yaxt="n", tck=0.0)
   yticks <- c(0.001, 0.01, 0.1, 1, 10, 100, 1000)
   xticks <- c(100, thou(1), thou(10), thou(100), mil(1))
   xlabels <- sci.suffix(xticks)
@@ -365,10 +384,11 @@ build.time.vs.rules <- function (dir = "./pdfs") {
   axis(2, at=yticks, labels = prettyNum(yticks, drop0trailing = T))
   mtext(2, text ="Table-build Time (s)", cex = cexsize, line = 2 )
   for( i in 2:length(rmgd)){
-    points(x = rmgd[[i]]$number.of.rules, y = rmgd[[i]]$table.build.time, pch = i+1)
+    points(x = rmgd[[i]]$number.of.rules, y = rmgd[[i]]$table.build.time,
+           pch = pchvals[i])
   }
   legend(x="bottomright",legend=c(32,104,320,12000),
-         pch=c(1,3,4,5), title="Bits Classified")
+         pch=pchvals, title="Bits Classified")
   dev.off()
 }
 
@@ -387,7 +407,9 @@ series.of.4.graph <- function(numrules
                               ,xaxis.line = 2
                               ,pps.line = 2
                               ,pps.adj = 0.5
-                              ,dir="./pdfs"){
+                              ,dir="./pdfs"
+                              ,height = device.height
+                              ){
   totals <- list(  loadin("alltest32bits_[1-3].csv")
                  , loadin("alltest104bits_[1-3].csv")
                  , loadin("alltest320bits_[1-3].csv")
@@ -396,16 +418,17 @@ series.of.4.graph <- function(numrules
   titletext <- paste("Throughputs for", comma(numrules),"Rules")
   pps.ed <- lapply(totals, add.real.pps)
   only <- lapply(pps.ed, function(x) subset(x, x$number.of.rules == numrules))
-  pdf(paste(dir,paste("4series_",dot(numrules),"_rules.pdf",sep=""),sep="/"),
-      height=8.5, width=11, paper="USr", title = titletext)
+  printdevice(paste(dir,paste("4series_",dot(numrules),"_rules",sep=""),sep="/"),
+             titletext)
   options(scipen = 10)# only shorten to scientific notation if difference is >
                       # 10 characters
   par(cex = cexsize , bty="u", mar = mar, ann = F)
   plot(x = only[[1]]$memory.used, y = only[[1]]$real.pps, log="x",
        xlim = xlimits, ylim = ylimits,
-       pch = 1,
+       pch = pchvals[1],
        xaxt="n",yaxt="n", tck=0.0
        )
+
   xlab <- "Memory Used"
   ppslabel <- "Throughput (PPS)"
   pps.ticks <- pretty(ylimits, 10)
@@ -420,57 +443,134 @@ series.of.4.graph <- function(numrules
   axis(4, at = pps.ticks, labels = pps.ticks * bpsmul, las = 1)
   mtext(4, text = bps.label, line = bps.line, cex = cexsize, adj = bps.adj)
   for( i in 2:length(only)){
-    points(x = only[[i]]$memory.used, y = only[[i]]$real.pps, pch = i+1)
+    points(x = only[[i]]$memory.used, y = only[[i]]$real.pps, pch = pchvals[i])
   }
   legend(x=wherelegend, legend=c(32,104,320,12000),
-         pch=c(1,3,4,5), title="Bits Classified")
+         pch=pchvals, title="Bits Classified")
   dev.off()
 }
 
-#specialized for 1K
-series.of.4.graph.1K <- function(){
-  series.of.4.graph(numrules = 1000
-                    ,xlimits = c(10000, 2000000000)
-                    ,ylimits = c(1000,  1750000)
-                    ,mar = c(3,4.1,3,3.8)
-                    ,pps.line = 3.1
-                    ,bps.line = 2.3
-                    ,bps.adj = 0.45
-                    )
+exp.4.graph <- function(numrules
+                        ,xlimits
+                        ,ylimits
+                        ,xlabels
+                        ,bps.label = "Throughput (Gbps)"
+                        ,bps.adj = 0.5
+                        ,bpsmul = 0.000012
+                        ,bps.line = 2
+                        ,xlas = 1
+                        ,mar = c(3,4,4,4)
+                        ,xaxticks = F
+                        ,wherelegend = "topright"
+                        ,xaxis.line = 2
+                        ,pps.line = 2
+                        ,pps.adj = 0.5
+                        ,dir="./pdfs"
+                        ,relheights = c(0.92,0.92)
+                        ){
+  totals <- list(  loadin("alltest32bits_[1-3].csv")
+                 , loadin("alltest104bits_[1-3].csv")
+                 , loadin("alltest320bits_[1-3].csv")
+                 , loadin.12K("maxtest_[1-3].csv")
+                 )
+  titletext <- paste("Throughputs for", comma(numrules),"Rules")
+  pps.ed <- lapply(totals, add.real.pps)
+  only <- lapply(pps.ed, function(x) subset(x, x$number.of.rules == numrules))
+  only.1 <- lapply(only, function(x) subset(x, x$real.pps <= ylimits[2]))
+  only.2 <- lapply(only, function(x) subset(x, x$real.pps >= ylimits[3]))
+  printdevice(paste(dir,paste("4series_",dot(numrules),"_rules",sep=""),sep="/"),
+             titletext)
+  options(scipen = 10)# only shorten to scientific notation if difference is >
+                      # 10 characters
+  par(cex = cexsize , bty="u", ann = F, mar=c(0,0,0,0))#, mar = mar
+  mrx <- c(mar[2], -mar[4], mar[1], -mar[3]) * 0.03
+  
+  pps.ticks1 <- pretty(ylimits[3:4], 1)
+  pps.ticks2 <- pretty(ylimits[1:2], 10)
+  #plot first group
+  par(fig=c(0, 1, relheights[2], 1) + c(mrx[1],mrx[2],0,mrx[4]), bty="n")
+  plot(x = only.2[[1]]$memory.used, y = only.2[[1]]$real.pps, log="x",
+       xlim = xlimits, ylim = ylimits[3:4], pch=pchvals[1],
+       xaxt="n",yaxt="n",tck=0.0)
+  for(i in 2:length(only.2)){
+    points(x = only.2[[i]]$memory.used, y = only.2[[i]]$real.pps, pch = pchvals[i])
+  }
+  axis(2, at = pps.ticks1, labels = sci.suffix(pps.ticks1), las=1)
+  axis(4, at = pps.ticks1, labels = pps.ticks1 * bpsmul, las = 1)
+  lines(x=c(xlimits[1]*0.5,xlimits[2]*1.6), y=rep(ylimits[3],2), lty="dashed")
+  #plot second group
+  par(fig=c(0, 1, 0, relheights[1]) + c(mrx[1],mrx[2],mrx[3],0), new=T,
+      lty="solid", bty="u")
+  plot(x = only.1[[1]]$memory.used, y = only.1[[1]]$real.pps, log="x",
+       xlim = xlimits, ylim = ylimits[1:2],
+       pch = pchvals[1],
+       xaxt="n",yaxt="n", tck=0.0
+       )
+
+  xlab <- "Memory Used"
+  ppslabel <- "Throughput (PPS)"
+  if(xaxticks[1] == F){
+    axis(1, at = axTicks(1,log=T), labels = byte.suffix(axTicks(1,log=T)), las = xlas)
+  }else{
+    axis(1, at = xaxticks, labels = byte.suffix(xaxticks), las = xlas)
+  }
+  mtext(1, text = xlab, line = xaxis.line , cex = cexsize)
+  axis(2, at = pps.ticks2, labels = sci.suffix(pps.ticks2), las=1)
+  mtext(2, text = ppslabel, line = pps.line, cex = cexsize, adj = pps.adj)
+  axis(4, at = pps.ticks2, labels = pps.ticks2 * bpsmul, las = 1)
+  mtext(4, text = bps.label, line = bps.line, cex = cexsize, adj = bps.adj)
+  for( i in 2:length(only.1)){
+    points(x = only.1[[i]]$memory.used, y = only.1[[i]]$real.pps, pch = pchvals[i])
+  }
+  legend(x=wherelegend, inset=0.02, legend=c(32,104,320,12000),
+         pch=pchvals, title="Bits Classified")
+
+  dev.off()
 }
 
-series.of.4.graph.10K <- function(){
-  series.of.4.graph(numrules = 10000
-                    ,xlimits = c(50000, 2000000000)
-                    ,ylimits = c(1, 230000)
-                    ,wherelegend = "bottomleft"
-                    ,mar = c(3,4,3,3.8)
-                    ,pps.line = 3
-                    ,bps.line = 2.8
-                    )
+exp.4.graph.1K <- function() {
+  exp.4.graph(numrules = thou(1)
+              ,xlimits = c(thou(10), bil(2))
+              ,ylimits = c(0, mil(1.78), mil(3.5), mil(4.5))
+              ,mar = c(4,4.1,0,3.8)
+              ,relheights = c(0.92,0.92)
+              ,pps.line = 3.1
+              ,bps.line = 2.3
+              ,bps.adj = 0.45
+              )
 }
 
-series.of.4.graph.100K <- function(){
-  series.of.4.graph(numrules = thou(100)
-                    ,xlimits = c(mil(1), bil(2))
-                    ,ylimits = c(1, thou(30))
-                    ,bpsmul = 0.012
-                    ,bps.label = "Throughput (Mbps)"
-                    ,xaxticks = c(mil(1), mil(3), mil(10), mil(30),
-                       mil(100), mil(300), bil(1))
-                    ,wherelegend = "bottomleft"
-                    ,xlas = 1
-                    ,xaxis.line = 2
-                    ,pps.line = 2.8
-                    ,bps.line = 2.6
-                    ,mar = c(3,3.8,3,3.6)
-                    )
+exp.4.graph.10K <- function(){
+  exp.4.graph(numrules = thou(10)
+              ,xlimits = c(thou(50), bil(2))
+              ,ylimits = c(1, thou(235), mil(2.1),mil(2.6))
+              ,wherelegend = "bottomleft"
+              ,mar = c(3.9,4,0,3.8)
+              ,pps.line = 3
+              ,bps.line = 2.8
+              )
+}
+
+exp.4.graph.100K <- function(){
+  exp.4.graph(numrules = thou(100)
+              ,xlimits = c(mil(1), bil(2))
+              ,xaxticks = c(mil(1),mil(3),mil(10),mil(30),mil(100),mil(300),bil(1))
+              ,ylimits = c(1, thou(30), thou(805), thou(860))
+              ,bpsmul = 0.012
+              ,bps.label = "Throughput (Mbps)"
+              ,wherelegend = "bottomleft"
+              ,xlas = 1
+              ,xaxis.line = 2
+              ,pps.line = 2.8
+              ,bps.line = 2.6
+              ,mar = c(4,3.8,0,3.6)
+              )
 }
 # convenience function for drawing all specialized graphs
 series.of.4.graph.all <- function(){
-  series.of.4.graph.100K()
-  series.of.4.graph.10K()
-  series.of.4.graph.1K()
+  exp.4.graph.100K()
+  exp.4.graph.10K()
+  exp.4.graph.1K()
 }
 
 #super convenient and slow
@@ -481,3 +581,92 @@ all.graphs <- function() {
   classifier.throughputs()
 }
 
+# axis.break places a break marker at the position "breakpos" 
+# in user coordinates on the axis nominated - see axis().
+
+axis.break<-function(axis=1,breakpos,bgcol="white",breakcol="black",
+ style=c("slash","zigzag"),brw=0.02) {
+ 
+ if(!missing(breakpos)) {
+  # get the coordinates of the outside of the plot
+  figxy<-par("usr")
+  # flag if either axis is logarithmic
+  xaxl<-par("xlog")
+  yaxl<-par("ylog")
+  # calculate the x and y offsets for the break
+  xw<-(figxy[2]-figxy[1])*brw
+  yw<-(figxy[4]-figxy[3])*brw
+  if(xaxl && (axis == 1 || axis == 3)) breakpos<-log10(breakpos)
+  if(yaxl && (axis == 2 || axis == 4)) breakpos<-log10(breakpos)
+  # set up the "blank" rectangle (left, bottom, right, top)
+  switch(axis,
+   br<-c(breakpos-xw/2,figxy[3]-yw/2,breakpos+xw/2,figxy[3]+yw/2),
+   br<-c(figxy[1]-xw/2,breakpos-yw/2,figxy[1]+xw/2,breakpos+yw/2),
+   br<-c(breakpos-xw/2,figxy[4]-yw/2,breakpos+xw/2,figxy[4]+yw/2),
+   br<-c(figxy[2]-xw/2,breakpos-yw/2,figxy[2]+xw/2,breakpos+yw/2),
+   stop("Improper axis specification."))
+  # get the current setting of xpd
+  old.xpd<-par("xpd")
+  # don't cut the break off at the edge of the plot
+  par(xpd=T)
+  # correct for logarithmic axes
+  if(xaxl) br[c(1,3)]<-10^br[c(1,3)]
+  if(yaxl) br[c(2,4)]<-10^br[c(2,4)]
+  # draw the "blank" rectangle
+  rect(br[1],br[2],br[3],br[4],col=bgcol,border=bgcol)
+  if(style == "slash") {
+   # calculate the slash ends
+   if(axis == 1 || axis == 3) {
+    xbegin<-c(breakpos-xw,breakpos)
+    xend<-c(breakpos,breakpos+xw)
+    ybegin<-c(br[2],br[2])
+    yend<-c(br[4],br[4])
+    if(xaxl) {
+     xbegin<-10^xbegin
+     xend<-10^xend
+    }
+   }
+   else {
+    xbegin<-c(br[1],br[1])
+    xend<-c(br[3],br[3])
+    ybegin<-c(breakpos-yw,breakpos)
+    yend<-c(breakpos,breakpos+yw)
+    if(yaxl) {
+     ybegin<-10^ybegin
+     yend<-10^yend
+    }
+   }
+  }
+  else {
+   # calculate the zigzag ends
+   if(axis == 1 || axis == 3) {
+    xbegin<-c(breakpos-xw/2,breakpos-xw/4,breakpos+xw/4)
+    xend<-c(breakpos-xw/4,breakpos+xw/4,breakpos+xw/2)
+    ybegin<-c(ifelse(yaxl,10^figxy[3+(axis==3)],figxy[3+(axis==3)]),br[4],br[2])
+    yend<-c(br[4],br[2],ifelse(yaxl,10^figxy[3+(axis==3)],figxy[3+(axis==3)]))
+    if(xaxl) {
+     xbegin<-10^xbegin
+     xend<-10^xend
+    }
+   }
+   else {
+    xbegin<-c(ifelse(xaxl,10^figxy[1+(axis==4)],figxy[1+(axis==4)]),br[1],br[3])
+    xend<-c(br[1],br[3],ifelse(xaxl,10^figxy[1+(axis==4)],figxy[1+(axis==4)]))
+    ybegin<-c(breakpos-yw/2,breakpos-yw/4,breakpos+yw/4)
+    yend<-c(breakpos-yw/4,breakpos+yw/4,breakpos+yw/2)
+    if(yaxl) {
+     ybegin<-10^ybegin
+     yend<-10^yend
+    }
+   }
+  }
+  # draw the segments
+  segments(xbegin,ybegin,xend,yend,col=breakcol,lty=1)
+  # restore xpd
+  par(xpd=old.xpd)
+ }
+ else {
+  cat("Usage: axis.break(axis=1,breakpos,bgcol=\"white\",breakcol=\"black\",\n")
+  cat("\tstyle=c(\"slash\",\"zigzag\"),brw=0.02)\n")
+ }
+}
